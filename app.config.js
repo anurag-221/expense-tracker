221f145c85f47f8bf7f68dbf24bc1c50f6ef27b0
anurag-ayasya <anurag.srivastava@ayasya.com>
2025-12-03 18:05:44 +0530

    Remove deprecated Android Gradle files and update app configuration
    - Deleted gradlew.bat and react-settings-plugin files as they are no longer needed.
    - Updated app.config.js and app.json to reflect new package name and permissions.
    - Updated package.json and package-lock.json to use the latest versions of dependencies.
    - Adjusted Android and iOS configurations for better compatibility and performance.

--- app.config.js
+++ app.config.js
@@ -1,31 +1,76 @@
 export default {
-  name: "Sapphire Ledger",
-  slug: "sapphire-ledger",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  scheme: "sapphireledger",
-  userInterfaceStyle: "automatic",
-  splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#EFE9FF"
-  },
-  android: {
-    package: "com.sapphire.ledger",
-    adaptiveIcon: {
-      foregroundImage: "./assets/icon.png",
-      backgroundColor: "#EFE9FF"
+  expo: {
+    name: "Sapphire Ledger",
+    slug: "sapphire-ledger",
+    owner: "anurags221s-organization",
+
+    version: "1.0.0",
+    orientation: "portrait",
+
+    icon: "./assets/icon.png",
+    scheme: "sapphireledger",
+    userInterfaceStyle: "automatic",
+
+    cli: {
+      appVersionSource: "remote"
     },
-    permissions: [
-      "READ_EXTERNAL_STORAGE",
-      "WRITE_EXTERNAL_STORAGE",
-      "NOTIFICATIONS"
-    ]
-  },
-  extra: {
-    eas: {
-      projectId: "54864cb5-947d-4dbf-9c6f-f3c16812d85e"
+
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#1A1132"
+    },
+
+    android: {
+      package: "com.anurag.sapphireledger",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#000000"
+      },
+      permissions: [
+        "INTERNET",
+        "USE_BIOMETRIC",
+        "USE_FINGERPRINT",
+        "CAMERA"
+      ]
+    },
+
+    ios: {
+      bundleIdentifier: "com.anurag.sapphireledger",
+      buildNumber: "1",
+      supportsTablet: false,
+      deploymentTarget: "15.1",
+      infoPlist: {
+        NSFaceIDUsageDescription:
+          "This app uses Face ID for secure access to your financial data.",
+        NSCameraUsageDescription: "Camera is used for scanning receipts.",
+        NSPhotoLibraryUsageDescription: "Photos are used to select receipts."
+      }
+    },
+
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          android: {
+            compileSdkVersion: 34,
+            targetSdkVersion: 34,
+            minSdkVersion: 23
+          },
+          ios: {
+            deploymentTarget: "15.1"
+          }
+        }
+      ],
+      "expo-local-authentication",
+      "expo-notifications"
+    ],
+
+    extra: {
+      eas: {
+        projectId: "54864cb5-947d-4dbf-9c6f-f3c16812d85e"
+      }
     }
   }
 };
